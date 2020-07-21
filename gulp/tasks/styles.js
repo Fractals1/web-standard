4fcbe0afef4c823a3f6635a0fcf7f30c2e98ba95
bolt00 <technology2000@mail.ru>
2020-07-22 00:20:56 +0300

    Desktop nav-menu complete

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,12 +1,12 @@
-const gulp = require('gulp')
-const plumber = require('gulp-plumber')
-const sass = require('gulp-sass')
-const cleanCSS = require('gulp-clean-css')
-const sourcemaps = require('gulp-sourcemaps')
-const shorthand = require('gulp-shorthand')
-const autoprefixer = require('gulp-autoprefixer')
-const gulpStylelint = require('gulp-stylelint')
-const rename = require("gulp-rename")
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const shorthand = require('gulp-shorthand');
+const autoprefixer = require('gulp-autoprefixer');
+const gulpStylelint = require('gulp-stylelint');
+const rename = require("gulp-rename");
 
 module.exports = function styles() {
   return gulp.src('src/styles/*.scss')
@@ -26,14 +26,13 @@ module.exports = function styles() {
       cascade: false
     }))
     .pipe(shorthand())
-    .pipe(cleanCSS({
+    /*.pipe(cleanCSS({
       debug: true,
       compatibility: '*'
     }, details => {
       console.log(`${details.name}: Original size:${details.stats.originalSize} - Minified size: ${details.stats.minifiedSize}`)
-    }))
+    }))*/
     .pipe(sourcemaps.write())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('build/css'))
-}
-
+    .pipe(gulp.dest('build/css'));
+};
